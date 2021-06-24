1911899430aa553ea34b5d248e64eb3fdf20026d
Lucasfield3 <lucasclic@yahho.com.br>
2021-06-23 22:58:16 -0300

    move-it basics done

--- src/context/ChallengesContext.tsx
+++ src/context/ChallengesContext.tsx
@@ -1,6 +1,7 @@
-import { createContext, ReactNode, useEffect, useState } from 'react'
+import React, { createContext, ReactNode, useEffect, useState } from 'react'
 import challenges from '../../challenges.json'
 import Cookies from 'js-cookie'
+import { LevelUpModal } from '../components/LevelUpModal'
 
 interface Challenge {
     type: 'body' | 'eye';
@@ -17,27 +18,38 @@ interface ChallengesContextData {
     levelUp:()=> void,
     startNewChallenge: ()=> void,
     resetChallenge:()=> void,
-    challengeCompleted:()=> void
+    challengeCompleted:()=> void,
+    closeLevelUpModal:()=> void
 }
 
 interface ChallengesProviderProps {
     children: ReactNode;
+    level:number;
+    currentExperience:number;
+    challengesCompleted:number;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData)
 
-export function ChallengesProvider ({children}:ChallengesProviderProps){
+export function ChallengesProvider ({children, ...rest}:ChallengesProviderProps){ //rest = todas as props menos a children
     
-    const [level, setLevel]  = useState(1)
-    const [currentExperience, setCurrenteExperience] = useState(0)
-    const [challengesCompleted, setChallengesCompleted] = useState(0)
+    const [level, setLevel]  = useState(rest.level ?? 0)
+    const [currentExperience, setCurrenteExperience] = useState(rest.currentExperience ?? 0)
+    const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0)
     const [activeChallenge, setActiveChallenge] = useState(null)
+
+    const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false)
 
     const experienceToTheNextLevel = Math.pow((level + 1 ) * 4, 2)
 
     function levelUp(){
         setLevel(level + 1)
+        setIsLevelUpModalOpen(true)
     }
+
+    function closeLevelUpModal (){
+        setIsLevelUpModalOpen(false)
+    } 
 
     function startNewChallenge(){
        const randomChallengesBox = Math.floor(Math.random() * challenges.length)
@@ -81,6 +93,12 @@ export function ChallengesProvider ({children}:ChallengesProviderProps){
         Notification.requestPermission()
     }, [])
 
+    useEffect(()=> {
+        Cookies.set('level', String(level))
+        Cookies.set('currentExperience', String(currentExperience))
+        Cookies.set('challengesCompleted', String(challengesCompleted))
+    }, [level, currentExperience, challengesCompleted])
+
     return(
         <ChallengesContext.Provider value={{
         level, 
@@ -91,9 +109,12 @@ export function ChallengesProvider ({children}:ChallengesProviderProps){
         activeChallenge,
         resetChallenge,
         experienceToTheNextLevel,
-        challengeCompleted 
+        challengeCompleted, 
+        closeLevelUpModal 
         }}>
             {children}
+
+            {isLevelUpModalOpen && <LevelUpModal/>}
         </ChallengesContext.Provider>
     )
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,26 +7,68 @@ import Head from 'next/head';
 import React from "react";
 import { ChallengeBox } from "../components/ChallengesBox";
 import { CountDownProvider } from "../context/CountDownContext";
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
-      <ExperienceBar/>
+import { GetServerSideProps } from 'next'
+import { ChallengesProvider } from "../context/ChallengesContext";
 
-      <CountDownProvider>
-        <section>
-          <div>
-            <Profile/>
-            <CompletedChalenges/>
-            <CountDown/>
-          </div>
-          <div style={{height: '100%'}}>
-            <ChallengeBox/>
-          </div>
-        </section>
-      </CountDownProvider>
-    </div>
+
+interface HomeProps {
+  level:number;
+  currentExperience:number;
+  challengesCompleted:number
+}
+
+export default function Home(props: HomeProps) {
+
+
+  return (
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
+        <ExperienceBar/>
+
+        <CountDownProvider>
+          <section>
+            <div>
+              <Profile/>
+              <CompletedChalenges/>
+              <CountDown/>
+            </div>
+            <div style={{height: '100%'}}>
+              <ChallengeBox/>
+            </div>
+          </section>
+        </CountDownProvider>
+      </div>
+    </ChallengesProvider>
   )
 }
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => { 
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  return{
+    props:{
+      level:Number(level),
+      currentExperience:Number(currentExperience),
+      challengesCompleted:Number(challengesCompleted)
+  }
+
+  }
+
+}
+
+//Essa função getServerSideProp esta rodando no nodejs e não no browser.
+// O por que dessa função?. Porque quando a  aplicação é requisitada na barra de pesquisa do google,
+// o google n vai ler essa função se essa função estiver rodando na camada do browser, por conta de ser
+// asincrona, mas ao colocar essa função na camada intermediária aonde fica o next, antes de renderizar o index
+// no front e requisição dessa função asincroina é feita antes, por tanto, as informações que essa função retorna
+// vão popular o index antes que o google leia a página, assim, os dados serão mostrados na página  
