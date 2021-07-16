e93a7f2689133b7037261963a6b3ca298f41b1e3
Lucasfield3 <lucasclic@yahho.com.br>
2021-07-15 22:56:56 -0300

    input edit cicle done

--- src/components/EditCicle.tsx
+++ src/components/EditCicle.tsx
@@ -1,24 +1,58 @@
-import { useContext } from 'react'
+import { useEffect } from 'react'
+import { ChangeEvent, useContext } from 'react'
+import { MenuButtonContext } from '../context/MenuButtonContext'
 import { SettingsContext } from '../context/SettingsContext'
 import styles from '../styles/components/EditCicle.module.css'
 
 
 export default function EditCicle(){
 
-
     const { setMinutesInput, setSecondsInput, minutesEdit, secondsEdit } = useContext(SettingsContext)
+
+    function handleMaxTimeLimitSecond(value:string){
+        if(Number(value) > 59) {
+            setSecondsInput('59')
+        }else if(isNaN(Number(value))){
+            setSecondsInput('')
+        }else{
+            setSecondsInput(value)
+        }
+        
+    }
+
+    function checkIsNaNMin(){
+        if(isNaN(Number(minutesEdit))){
+            setMinutesInput('')
+        }
+    }
+
+    function checkIsNaNSec(){
+        if(isNaN(Number(secondsEdit))){
+            setSecondsInput('')
+        }
+    }
+
+    useEffect(()=> {
+        checkIsNaNMin()
+        checkIsNaNSec()
+    }, [])
+
+    function handleMaxTimeLimitMinute(value:string){
+        if(Number(value) > 99) {
+            setMinutesInput('99')
+        }else if(isNaN(Number(value))){
+            setMinutesInput('')
+        }else{
+            setMinutesInput(value)
+        }
+        
+    }
 
     return(
         <div className={styles.timer}>
-            <input value={minutesEdit}  onChange={(e)=>{
-               setMinutesInput(e.target.value)
-                console.log(e.target.value)
-                }} max={99} pattern='\d*' min={1}  type='number'/>
+            <input value={minutesEdit} maxLength={2} placeholder='00' onChange={(e)=>handleMaxTimeLimitMinute(e.target.value)}   type='text'/>
             <p>:</p>
-            <input value={secondsEdit}  onChange={(e)=>{
-                setSecondsInput(e.target.value)
-                console.log(e.target.value)
-                }} max={59} pattern='\d*' min={1}  type='number'/>
+            <input value={secondsEdit} maxLength={2} placeholder='00' onChange={(e)=>handleMaxTimeLimitSecond(e.target.value)}   type='text'/>
         </div>
     )
 
